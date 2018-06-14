33e39ca76454568abddd612ae71c956e6687de44
Jin Huang <jinhuang696@gmail.com>
2018-06-13 23:29:27 -0700

    project gui

--- BranchUI.java
+++ BranchUI.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.*;
 
-public class BranchUI {
-    System.out.println("hi")
+@SuppressWarnings("serial")
+public class BranchUI extends JFrame{
+    static BranchUI branchUI ;
+
+    final int WIDTH = 500, HEIGHT = 1000;
+    public BranchUI() {
+        setSize(WIDTH, HEIGHT);
+        setTitle("Branch.exe");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        draw();
+    }
+
+    public void draw() {
+        // TODO
+    }
 }
