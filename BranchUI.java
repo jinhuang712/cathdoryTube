86594775e9705bf1661069e9501fd095f9338aba
Jin Huang <jinhuang696@gmail.com>
2018-06-13 23:45:36 -0700

    project gui

--- BranchUI.java
+++ BranchUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class BranchUI extends JFrame{
     static BranchUI branchUI ;
 
-    final int WIDTH = 500, HEIGHT = 1000;
+    final int WIDTH = 500, HEIGHT = 300;
     public BranchUI() {
         setSize(WIDTH, HEIGHT);
         setTitle("Branch.exe");
---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        BranchUI branchUI = new BranchUI();
+        branchUI.setResizable(false);
+        branchUI.setVisible(true);
+    }
+}
